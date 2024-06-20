2281910829cb9fcdb3d57a7440d75ad97eda35dd
opk <opk@example.com>
2024-06-20 11:29:55 -0600

    Edited

--- delete.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-helm delete $(helm list | grep -e  api  -e web   -e postgres -e redis | awk '{print $1}' 
-)
-
-kubectl delete pvc --all
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-helm install  postgres  postgresql
-helm install redis  redis 
-helm install  api thunder-api 
-helm install  web thunder-web
